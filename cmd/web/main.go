cc930d05b09b9d92d1622389d7538117b2484593
anras5 <anras1filip@gmail.com>
2022-10-23 17:05:46 +0200

    Add helpers (error handling) package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,10 +6,12 @@ import (
 	"github.com/alexedwards/scs/v2"
 	"github.com/anras5/go_bookings/internal/config"
 	"github.com/anras5/go_bookings/internal/handlers"
+	"github.com/anras5/go_bookings/internal/helpers"
 	"github.com/anras5/go_bookings/internal/models"
 	"github.com/anras5/go_bookings/internal/render"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -17,6 +19,8 @@ const portNumber = ":8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -43,6 +47,12 @@ func run() error {
 	// change this to true when in production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
@@ -63,6 +73,7 @@ func run() error {
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
 	render.NewTemplate(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
