43cf514dac98a7bd333954fae045cc0c94f22719
anras5 <anras1filip@gmail.com>
2022-12-06 23:24:07 +0100

    Add migration seed rooms

---
+++ migrations/20221206220026_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
+                                                               ('General''s Quarters','2021-08-09 00:00:00','2021-08-09 00:00:00'),
+                                                               ('Majors Suite','2022-10-30 00:00:00','2022-10-30 00:00:00');
